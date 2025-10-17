9a5c490fa1159b453796b0342c15372978ca2fce
unitybighead <unitybighead@gmail.com>
2025-10-17 10:55:19 +0900

    typo

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,4 @@
-import {Inter, Lusinata} from 'next/font/google';
-import { Lusitana } from 'next/font/google';
+import {Inter, Lusitana} from 'next/font/google';
 
 export const inter = Inter({subsets: ['latin']});
 export const lusitana = Lusitana({
